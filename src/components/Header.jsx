89764ba9888c027d58a578b8e2aebcdb4e32e019
Anton <solonovicanton@gmail.com>
2020-12-01 16:50:02 +0200

    Initial commit.

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header =()=>{
+return <header className='header'>
+            <img src="https://dt2sdf0db8zob.cloudfront.net/wp-content/uploads
+            /2019/08/10-Best-Gaming-Team-Logos-and-How-to-Make-Your-Own-CurrentYear
+            -image1-1.png"/>
+      </header>
+    
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Profile =()=>{
+    return  <div className='content'>
+                    <div>
+                         <img src = "https://pm1.narvii.com/6583/3f00498b7a46cfbf28503081894288e9cb9a0fae_00.jpg"/>
+                    </div>
+                    <div>
+                        ava + description
+                    </div>
+                    <div>
+                        My post
+                        <div>
+                        New post
+                        </div>
+                            <div>
+                            Post 1
+                            </div>
+                            <div>
+                            Post 2
+                            </div>
+                    </div>
+            </div>
+           
+} 
+
+export default Profile;
