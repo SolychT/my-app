cb796ec6e50a25952164aae4a891e1fa1b1d2af6
Anton <solonovicanton@gmail.com>
2020-12-14 20:31:10 +0200

    1 circle

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import s from './Navbar.module.css';
 
-// let s = {
-//     'nav': 'Navbar_nav__l62eJ',
-//     'item': 'Navbar_item__q4Q3t'
-// }
 
 const Navbar = () => {
     return <nav className={s.nav}>
